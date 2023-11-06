568e44a61633f782493c972245c6a6e15549526c
Paul Balanca <paulb@graphcore.ai>
2023-11-06 11:49:58 +0000

    Implement basic `ScaledArray` conversion semantics.
    Implementing `to_array` function to convert a ScaledArray to represented values.
    Note: supporting a custom dtype, to avoid potential underflowing/overflowing in
    case of conversion to more accurate floating point format.

--- tests/core/test_datatype.py
+++ tests/core/test_datatype.py
@@ -1,26 +1,52 @@
 # Copyright (c) 2023 Graphcore Ltd. All rights reserved.
 import chex
-import jax
 import jax.numpy as jnp
 import numpy as np
+import numpy.testing as npt
+from absl.testing import parameterized
 
 from jax_scaled_arithmetics.core import ScaledArray
 
 
-class IpuTileArrayPrimitiveTests(chex.TestCase):
-    def test__scaled_array__init__jax(self):
-        sarr = ScaledArray(data=jnp.array([1.0, 2.0]), scale=jnp.array(1))
-        assert isinstance(sarr.data, jax.Array)
-        assert isinstance(sarr.scale, jax.Array)
-        assert sarr.scale.shape == ()
-
-    def test__scaled_array__init__numpy(self):
-        sarr = ScaledArray(data=np.array([1.0, 2.0]), scale=np.array(1))
-        assert isinstance(sarr.data, np.ndarray)
-        assert isinstance(sarr.scale, np.ndarray)
+class ScaledArrayDataclassTests(chex.TestCase):
+    @parameterized.parameters(
+        {"npb": np},
+        {"npb": jnp},
+    )
+    def test__scaled_array__init__multi_numpy_backend(self, npb):
+        sarr = ScaledArray(data=npb.array([1.0, 2.0], dtype=np.float32), scale=npb.array(1))
+        assert isinstance(sarr.data, npb.ndarray)
+        assert isinstance(sarr.scale, npb.ndarray)
         assert sarr.scale.shape == ()
 
     def test__scaled_array__basic_properties(self):
         sarr = ScaledArray(data=jnp.array([1.0, 2.0]), scale=jnp.array(1))
         assert sarr.dtype == np.float32
         assert sarr.shape == (2,)
+
+    @parameterized.parameters(
+        {"npb": np},
+        {"npb": jnp},
+    )
+    def test__scaled_array__to_array__multi_numpy_backend(self, npb):
+        sarr = ScaledArray(data=npb.array([1.0, 2.0], dtype=np.float16), scale=npb.array(3))
+        # No dtype specified.
+        out = sarr.to_array()
+        assert isinstance(out, npb.ndarray)
+        assert out.dtype == sarr.dtype
+        npt.assert_array_equal(out, sarr.data * sarr.scale)
+        # Custom dtype.
+        out = sarr.to_array(dtype=np.float32)
+        assert isinstance(out, npb.ndarray)
+        assert out.dtype == np.float32
+        npt.assert_array_equal(out, sarr.data * sarr.scale)
+
+    @parameterized.parameters(
+        {"npb": np},
+        {"npb": jnp},
+    )
+    def test__scaled_array__numpy_array_interface(self, npb):
+        sarr = ScaledArray(data=npb.array([1.0, 2.0], dtype=np.float32), scale=npb.array(3))
+        out = np.asarray(sarr)
+        assert isinstance(out, np.ndarray)
+        npt.assert_array_equal(out, sarr.data * sarr.scale)
