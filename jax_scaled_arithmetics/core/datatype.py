926218a767725a3b65df04ddf11edd4b98a92f4e
Paul Balanca <paulb@graphcore.ai>
2023-11-07 14:27:53 +0000

    Implement basic ScaledArray conversion semantics. (#4)
    * Implement basic `ScaledArray` conversion semantics.
    
    Implementing `to_array` function to convert a ScaledArray to represented values.
    Note: supporting a custom dtype, to avoid potential underflowing/overflowing in
    case of conversion to more accurate floating point format.
    
    * Remove `Optional` typing scale in `ScaledArray`.
    
    Additionally, adding an `int8` unit test for `to_array`.

--- jax_scaled_arithmetics/core/datatype.py
+++ jax_scaled_arithmetics/core/datatype.py
@@ -1,12 +1,12 @@
 # Copyright (c) 2023 Graphcore Ltd. All rights reserved.
 from dataclasses import dataclass
-from typing import Optional, Union
+from typing import Any, Union
 
 import jax
 import numpy as np
 from chex import Shape
 from jax.tree_util import register_pytree_node_class
-from numpy.typing import DTypeLike
+from numpy.typing import DTypeLike, NDArray
 
 GenericArray = Union[jax.Array, np.ndarray]
 
@@ -19,14 +19,23 @@ class ScaledArray:
     JAX Scaled Arithmetics provides a consistent JAX LAX implementation
     propagating scaling for low-precision arithmetics.
 
+    Semantics: `ScaledArray` represents an array with the following values:
+        self.data * self.scale
+    where `self.scale` is always assumed to be broadcastable to `self.data`.
+
+    Notes:
+        1. Current implementation only supports `scale` being a scalar.
+        2. `data` and `scale` can have different dtypes. `data` dtype is used as the
+            reference dtype. Meaning a power of 2 `scale` is just a dtype `E8M0` for instance.
+
     Args:
         data: Un-scaled data array.
-        scale: Scale array (scalar only supported).
+        scale: Scale array (scalar only supported at the moment).
             If `scale` is None, equivalent to a normal array.
     """
 
     data: GenericArray
-    scale: Optional[GenericArray] = None
+    scale: GenericArray
 
     def __post_init__(self):
         assert isinstance(self.data, (jax.Array, np.ndarray))
@@ -53,3 +62,19 @@ class ScaledArray:
     @property
     def shape(self) -> Shape:
         return self.data.shape
+
+    def to_array(self, dtype: DTypeLike = None) -> GenericArray:
+        """Convert to the scaled array to a Numpy/JAX array.
+
+        Args:
+            dtype: Optional conversion dtype. `data.dtype` by default.
+        """
+        dtype = self.data.dtype if dtype is None else dtype
+        data = self.data.astype(dtype)
+        scale = self.scale.astype(dtype)
+        values = data * scale
+        return values
+
+    def __array__(self, dtype: DTypeLike = None) -> NDArray[Any]:
+        """Numpy array interface support."""
+        return np.asarray(self.to_array(dtype))
