d476f74ac47b855134ecdbb2c8c6c0cc1aefc89e
Paul Balanca <paulb@graphcore.ai>
2023-11-16 16:21:10 +0000

    Expand unit testing of `autoscale` interpreter decorator. (#19)
    Using parameterized testing to easily extend test coverage.
    Allowed catching an issue with scalar Numpy constants & output JAX PyTree.

--- jax_scaled_arithmetics/core/__init__.py
+++ jax_scaled_arithmetics/core/__init__.py
@@ -1,3 +1,3 @@
 # Copyright (c) 2023 Graphcore Ltd. All rights reserved.
-from .datatype import DTypeLike, ScaledArray, Shape, scaled_array  # noqa: F401
+from .datatype import DTypeLike, ScaledArray, Shape, is_scaled_leaf, scaled_array  # noqa: F401
 from .interpreters import autoscale, register_scaled_lax_op, register_scaled_op  # noqa: F401
