b5920b7ef47846e4cb37094a89bf49d127b4d802
Paul Balanca <paulb@graphcore.ai>
2023-11-17 16:03:10 +0000

    Implement `set_scaling` and `stop_scaling` JAX primitives. (#20)
    * `set_scaling`: Set the scaling of a tensor, transforming into `ScaledArray` in `autoscale` mode.
    * `stop_scaling`: Stop scale propagation of a tensor, transforming back into a JAX array.
    
    Both operations are no-op identity operations in normal JAX mode.
    
    Note: as pointed by @DouglasOrr, these primitives could also be formalized as casting operations, where ScaledDtypes are
    properly defined. To be clarified whether it may be a better setting for the JAX implementation!

--- jax_scaled_arithmetics/lax/__init__.py
+++ jax_scaled_arithmetics/lax/__init__.py
@@ -1,2 +1,3 @@
 # Copyright (c) 2023 Graphcore Ltd. All rights reserved.
+from .base_scaling_primitives import set_scaling, set_scaling_p, stop_scaling, stop_scaling_p  # noqa: F401
 from .scaled_ops import *  # noqa: F401, F403
---
+++ tests/lax/test_base_scaling_primitives.py
@@ -0,0 +1,68 @@
+# Copyright (c) 2023 Graphcore Ltd. All rights reserved.
+import chex
+import jax
+import jax.numpy as jnp
+import numpy as np
+import numpy.testing as npt
+
+from jax_scaled_arithmetics.core import ScaledArray, autoscale, scaled_array
+from jax_scaled_arithmetics.lax import set_scaling, stop_scaling
+
+
+class SetScalingPrimitiveTests(chex.TestCase):
+    @chex.variants(with_jit=True, without_jit=True)
+    def test__set_scaling_primitive__proper_result_without_autoscale(self):
+        def fn(arr, scale):
+            return set_scaling(arr, scale)
+
+        fn = self.variant(fn)
+        arr = jnp.array([2, 3], dtype=np.float32)
+        scale = jnp.array(4, dtype=np.float32)
+        out = fn(arr, scale)
+        npt.assert_array_equal(out, arr)
+
+    @chex.variants(with_jit=True, without_jit=True)
+    def test__set_scaling_primitive__proper_result_with_autoscale(self):
+        def fn(arr, scale):
+            return set_scaling(arr, scale)
+
+        fn = self.variant(autoscale(fn))
+        arr = scaled_array([-1.0, 2.0], 1.0, dtype=np.float32)
+        # TODO: support scalar here!
+        scale = scaled_array(1.0, 4.0, dtype=np.float32)
+        out = fn(arr, scale)
+        # Unchanged output tensor!
+        assert isinstance(out, ScaledArray)
+        npt.assert_array_equal(out.scale, scale)
+        npt.assert_array_equal(out, arr)
+
+
+class StopScalingPrimitiveTests(chex.TestCase):
+    @chex.variants(with_jit=True, without_jit=True)
+    def test__stop_scaling_primitive__proper_result_without_autoscale(self):
+        def fn(arr):
+            # Testing both variants.
+            return stop_scaling(arr), stop_scaling(arr, dtype=np.float16)
+
+        arr = jnp.array([2, 3], dtype=np.float32)
+        out0, out1 = self.variant(fn)(arr)
+        assert out0.dtype == arr.dtype
+        assert out1.dtype == np.float16
+        npt.assert_array_equal(out0, arr)
+        npt.assert_array_almost_equal(out1, arr)
+
+    @chex.variants(with_jit=True, without_jit=True)
+    def test__stop_scaling_primitive__proper_result_with_autoscale(self):
+        def fn(arr):
+            # Testing both variants.
+            return stop_scaling(arr), stop_scaling(arr, dtype=np.float16)
+
+        fn = self.variant(autoscale(fn))
+        arr = scaled_array([-1.0, 2.0], 3.0, dtype=np.float32)
+        out0, out1 = fn(arr)
+        assert isinstance(out0, jax.Array)
+        assert isinstance(out1, jax.Array)
+        assert out0.dtype == arr.dtype
+        assert out1.dtype == np.float16
+        npt.assert_array_equal(out0, arr)
+        npt.assert_array_almost_equal(out1, arr)
