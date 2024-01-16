bd94d04ecf2ad1d88407ffaee29d9784e914a730
Paul Balanca <paulb@graphcore.ai>
2024-01-16 11:16:36 +0000

    Add `epsilon` parameter to dynamic rescale operations.
    Improve stability of the MNIST training with large initialization as well as large step size.

--- tests/ops/test_rescaling.py
+++ tests/ops/test_rescaling.py
@@ -2,6 +2,7 @@
 import chex
 import numpy as np
 import numpy.testing as npt
+from absl.testing import parameterized
 
 from jax_scaled_arithmetics.core import ScaledArray, scaled_array
 from jax_scaled_arithmetics.ops import dynamic_rescale_l1, dynamic_rescale_l2, dynamic_rescale_max
@@ -36,3 +37,15 @@ class DynamicRescaleOpsTests(chex.TestCase):
         assert arr_out.dtype == arr_in.dtype
         npt.assert_array_equal(arr_out.scale, np.float16(16))
         npt.assert_array_equal(arr_out, arr_in)
+
+    @parameterized.parameters(
+        {"dynamic_rescale_fn": dynamic_rescale_max},
+        {"dynamic_rescale_fn": dynamic_rescale_l1},
+        {"dynamic_rescale_fn": dynamic_rescale_l2},
+    )
+    def test__dynamic_rescale__epsilon_norm_value(self, dynamic_rescale_fn):
+        arr_in = scaled_array([0, 0], np.float32(1), dtype=np.float16)
+        arr_out = dynamic_rescale_fn(arr_in)
+        # Rough bounds on the epsilon value.
+        assert arr_out.scale > 0.0
+        assert arr_out.scale < 0.001
