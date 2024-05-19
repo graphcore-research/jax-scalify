cef691c7b2581a9113770942f7b15d76e6fb1b34
Paul Balanca <paulb@graphcore.ai>
2024-05-19 22:25:53 +0100

    Fix JSA Python package building.

--- jax_scaled_arithmetics/__init__.py
+++ jax_scaled_arithmetics/__init__.py
@@ -1,5 +1,5 @@
 # Copyright (c) 2023 Graphcore Ltd. All rights reserved.
-from . import lax, ops
+from . import core, lax, ops
 from ._version import __version__
 from .core import (  # noqa: F401
     AutoScaleConfig,
